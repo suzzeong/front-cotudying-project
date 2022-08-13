dbeee503cea154196ede18b22b94f717f20a7728
suzzeong <su981006@naver.com>
2022-08-14 03:16:06 +0900

    로그인 유효성검사 수정중

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,89 +9,95 @@ import Logo from './Logo';
 const Login = () => {
   const navigate = useNavigate();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [disabled, setDisabled] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const initialValues = { email: '', password: '' };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
-  // useEffect(() => {
-  //   setDisabled(!(email && password && !errorMessage));
-  // }, [email, password, errorMessage]);
-
-  // 공백 제거
-  const removeWhitespace = (mail) => {
-    const regex = /\s/g;
-    // return mail.replace(regex, '');
-    console.log(mail)
-  };
-  
-
-  // email형식 확인
-  const validateEmail = (email) => {
-    removeWhitespace(email)
-    const regex =
-      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
-    return regex.test(email);
+  const handleChange = (e) => {
+    // console.log(e.target);
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+    // console.log(formValues);
   };
 
-  const handleInputEmail = (e) => {
-    // const changedEmail = removeWhitespace(email);
-    // setEmail(changedEmail);
-    // setErrorMessage(
-    //   validateEmail(changedEmail) ? '' : 'Please verify your email'
-    // );
-    setEmail(e.target.value);
-    console.log(e.target);
-  };
-  console.log(email);
-
-  // const handleInputPassword = (password) => {
-  //   // setPassword(removeWhitespace(password));
-  // };
-
-  const handleSigninBtnPress = (email) => {
-    console.log(validateEmail(email));
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
   };
 
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+
+  console.log(formErrors)
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    // const regex =
+    //   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
+    if (!values.email) {
+      errors.email = '이메일을 입력해주세요.';
+    } else if (!regex.test(values.email)) {
+      errors.email = '이메일 형식에 맞춰 입력해주세요.';
+    }
+    if (!values.password) {
+      errors.password = '비밀번호를 입력해주세요.';
+    } else if (values.password.length < 4) {
+      errors.password = '비밀번호를 4글자 이상 입력해주세요.';
+    }
+    return errors;
+  };
+  //(<pre>{JSON.stringify(formValues, undefined, 2)}</pre>)
   return (
     <LoginContainer>
       <Logo />
       <LoginTitle>로그인</LoginTitle>
-      <LoginForm>
+      <LoginForm onSubmit={handleSubmit}>
+        {Object.keys(formErrors).length === 0 && isSubmit ? (
+          <div>로그인에 성공했습니다.</div>
+        ) : (
+          <></>
+        )}
         <div>
           <EmailInput>
             <CommonInput
-              type='email'
-              value={email}
-              name='user_id'
-              onChange={handleInputEmail}
+              type='text'
+              value={formValues.email}
+              name='email'
+              onChange={handleChange}
               label='Email'
               placeholder='Email'
               margin='0 0 20px 0'
             />
           </EmailInput>
+          <p>{formErrors.email}</p>
           <PasswordInput>
             <CommonInput
               type='password'
-              value={password}
-              name='user_pw'
-              // onChange={handleInputPassword}
+              value={formValues.password}
+              name='password'
+              onChange={handleChange}
               label='Password'
               placeholder='Password'
               margin='0 0 20px 0'
             />
+            <p>{formErrors.password}</p>
           </PasswordInput>
         </div>
         <LoginButton>
           <CommonButton
-            type='button'
+            type='submit'
             bgcolor={colors.primary}
             fontcolor={colors.white}
             width='100%'
             height='40px'
             text='로그인'
-            onClick={() => {handleSigninBtnPress()}}
-            disabled={disabled}
+            // disabled={disabled}
           />
           <CommonButton
             onClick={() => {
@@ -111,7 +117,7 @@ const Login = () => {
 
 export default Login;
 
-const LoginContainer = styled.div`
+const LoginContainer = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -126,7 +132,7 @@ const LoginTitle = styled.h1`
   font-size: 30px;
 `;
 
-const LoginForm = styled.form`
+const LoginForm = styled.div`
   background-color: ${colors.white};
   border-radius: 10px;
   width: 500px;
