ccaade9053f2bb05c562521322e36b0197a531c8
suzzeong <su981006@naver.com>
2022-08-14 13:51:29 +0900

    커밋내용

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { colors } from '../theme/theme';
 import CommonButton from './elements/CommonButton';
@@ -6,28 +6,80 @@ import CommonInput from './elements/CommonInput';
 import Logo from './Logo';
 
 const Signup = () => {
-  const [inputId, setInputId] = useState('');
-  const [inputPw, setInputPw] = useState('');
+  const initialValues = { id: '', email: '', password: '', passwordcheck: '' };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
-  const handleInputId = (e) => {
-    setInputId(e.target.value);
+  const handleChange = (e) => {
+    // console.log(e.target);
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+    // console.log(formValues);
   };
 
-  const handleInputPw = (e) => {
-    setInputPw(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    // const regex =
+    //   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
+    if (!values.id) {
+      errors.id = '이름을 입력해주세요.';
+    }
+    if (!values.email) {
+      errors.email = '이메일을 입력해주세요.';
+    } else if (!regex.test(values.email)) {
+      errors.email = '이메일 형식에 맞춰 입력해주세요.';
+    }
+    if (!values.password) {
+      errors.password = '비밀번호를 입력해주세요.';
+    } else if (values.password.length < 4) {
+      errors.password = '비밀번호를 4글자 이상 입력해주세요.';
+    }
+    if (!values.passwordcheck) {
+      errors.passwordcheck = '비밀번호를 다시 입력해주세요.';
+    } else if (values.password !== values.passwordcheck) {
+      errors.passwordcheck = '비밀번호가 일치하지 않습니다.';
+    }
+    return errors;
   };
 
   return (
-    <LoginContainer>
+    <SignupContainer>
       <Logo />
-      <LoginTitle>회원가입</LoginTitle>
-      <LoginForm>
+      <SignupTitle>회원가입</SignupTitle>
+      <SignupForm onSubmit={handleSubmit}>
         <div>
           <EmailInput>
             <CommonInput
-              value={inputId}
-              name='user_id'
-              onChange={handleInputId}
+              type='text'
+              value={formValues.id}
+              name='id'
+              onChange={handleChange}
+              label='Id'
+              placeholder='Id'
+              margin='0 0 20px 0'
+            />
+          </EmailInput>
+          <EmailInput>
+            <CommonInput
+              type='text'
+              value={formValues.email}
+              name='email'
+              onChange={handleChange}
               label='Email'
               placeholder='Email'
               margin='0 0 20px 0'
@@ -35,32 +87,45 @@ const Signup = () => {
           </EmailInput>
           <PasswordInput>
             <CommonInput
-              value={inputPw}
-              name='user_pw'
-              onChange={handleInputPw}
+              type='password'
+              value={formValues.password}
+              name='password'
+              onChange={handleChange}
               label='Password'
               placeholder='Password'
               margin='0 0 20px 0'
             />
           </PasswordInput>
+          <PasswordCheckInput>
+            <CommonInput
+              type='password'
+              value={formValues.passwordcheck}
+              name='passwordcheck'
+              onChange={handleChange}
+              label='Confirm password'
+              placeholder='Confirm password'
+              margin='0 0 20px 0'
+            />
+          </PasswordCheckInput>
         </div>
         <LoginButton>
           <CommonButton
+            type='submit'
             bgcolor={colors.primary}
             fontcolor={colors.white}
-            // width='100%'
+            width='100%'
             height='40px'
             text='회원가입'
           />
         </LoginButton>
-      </LoginForm>
-    </LoginContainer>
+      </SignupForm>
+    </SignupContainer>
   );
 };
 
 export default Signup;
 
-const LoginContainer = styled.div`
+const SignupContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -68,18 +133,18 @@ const LoginContainer = styled.div`
   height: 100%;
 `;
 
-const LoginTitle = styled.h1`
+const SignupTitle = styled.h1`
   color: ${colors.white};
   margin: 40px 0;
   font-weight: 700;
   font-size: 30px;
 `;
 
-const LoginForm = styled.form`
+const SignupForm = styled.form`
   background-color: ${colors.white};
   border-radius: 10px;
   width: 500px;
-  height: 300px;
+  height: auto;
   padding: 30px 50px;
   display: flex;
   flex-direction: column;
@@ -89,6 +154,8 @@ const LoginForm = styled.form`
 const EmailInput = styled.div``;
 
 const PasswordInput = styled.div``;
+
+const PasswordCheckInput = styled.div``;
 
 const LoginButton = styled.div`
   display: flex;
